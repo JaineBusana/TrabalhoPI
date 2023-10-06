4c3a28ad6ffac2816f329f852956129a296d54d0
Gustavo Luís Pandini <gugui.pandini@gmail.com>
2023-10-05 21:50:40 -0300

    Classe Residuo e adcionei atributos em Coletor

--- Trabalho PI/Coletor.cs
+++ Trabalho PI/Coletor.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace Trabalho_PI
 {
-    internal class Coletor
+    public class Coletor
     {
+        //A class Coletor herda do Usuario Nome, Telefone, Sexo, Data de Nascimento, e-mail, usuário e senha
+        public static double CPF { get; set; }
+        public double CNPJ { get; set;}
+        public string endereco { get; set; }
+        public string formaDaColeta { get; set; }
+        public string descricao { get; set; }
+
+        private static List<Coletor> ListaColetor;
+
+        public static void Poupular()
+        {
+            Coletor novoColetor = new Coletor();
+            Console.WriteLine("Deseja realizar o cadastro como Pessoa Física ou Jurídica (1 = Física, 2 = Jurídica)");
+            int respTipoPessoa = Convert.ToInt32(Console.ReadLine());
+            if (respTipoPessoa == 1)
+            {
+
+            }
+            if (respTipoPessoa == 2)
+            {
+
+            }
+            Console.Write("Digite o seu CPF = ");
+            Coletor.CPF = Convert.ToDouble(Console.ReadLine());
+        }
+
     }
 }
