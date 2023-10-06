3427492a295c9fbf937a34386293e96d03845bd2
Gustavo Luís Pandini <gugui.pandini@gmail.com>
2023-10-06 20:11:13 -0300

    Popular e listar do coletor

--- Trabalho PI/Coletor.cs
+++ Trabalho PI/Coletor.cs
@@ -10,29 +10,55 @@ namespace Trabalho_PI
     {
         //A class Coletor herda do Usuario Nome, Telefone, Sexo, Data de Nascimento, e-mail, usuário e senha
         public static double CPF { get; set; }
-        public double CNPJ { get; set;}
-        public string endereco { get; set; }
-        public string formaDaColeta { get; set; }
-        public string descricao { get; set; }
-
-        private static List<Coletor> ListaColetor;
+        public static double CNPJ { get; set;}
+        public static string endereco { get; set; }
+        public static string formaDaColeta { get; set; }
+        public static string descricao { get; set; }
 
         public static void Poupular()
         {
-            Coletor novoColetor = new Coletor();
+            
             Console.WriteLine("Deseja realizar o cadastro como Pessoa Física ou Jurídica (1 = Física, 2 = Jurídica)");
             int respTipoPessoa = Convert.ToInt32(Console.ReadLine());
             if (respTipoPessoa == 1)
             {
-
+                Console.Write("Digite o seu CPF = ");
+                CPF = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Digite o seu Endereço = ");
+                endereco = Console.ReadLine();
+                Console.Write("Digite a forma de coleta = ");
+                formaDaColeta = Console.ReadLine();
+                Console.Write("Digite uma descrição = ");
+                descricao = Console.ReadLine();
+                Console.WriteLine("Coletor cadastrado!");
             }
             if (respTipoPessoa == 2)
             {
-
+                Console.Write("Digite o seu CNPJ = ");
+                CNPJ = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Digite o seu Endereço = ");
+                endereco = Console.ReadLine();
+                Console.Write("Digite a forma de coleta = ");
+                formaDaColeta = Console.ReadLine();
+                Console.Write("Digite uma descrição = ");
+                descricao = Console.ReadLine();
+                Console.WriteLine("Coletor cadastrado!");
             }
-            Console.Write("Digite o seu CPF = ");
-            Coletor.CPF = Convert.ToDouble(Console.ReadLine());
+            if (respTipoPessoa != 1 && respTipoPessoa != 2)
+            {
+                Console.Clear();
+                Console.WriteLine("Resposta invalida! ");
+                Console.WriteLine();
+                Poupular();
+            }
+            
         }
-
+        public static void ListarColetor()
+        {
+            Console.WriteLine($"CPF {CPF}");
+            Console.WriteLine($"Enderço {endereco}");
+            Console.WriteLine($"Forma da Coleta {formaDaColeta}");
+            Console.WriteLine($"Descrição {descricao}");
+        }
     }
 }
