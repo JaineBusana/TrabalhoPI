815fa9708e1c4ff1ec3daee1401033a9edef45de
Jaine Edna Bento Busana <jaine.ebbusanaf@gmail.com>
2023-10-20 19:33:59 -0300

    baixei o dapper e o MySQL e criei a conexão com o banco TRABALHO_PI

--- Trabalho_PI/ConsoleHelpers/Database.cs
+++ Trabalho_PI/ConsoleHelpers/Database.cs
@@ -1,13 +1,29 @@
 ﻿using System;
+using Dapper;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Trabalho_PI.ConsoleHelpers
 {
     public class Database
     {
+        protected string conectionString = "Server=localhost;Database=TRABALHO_PI;User=root;Password=root;";
 
+
+        protected MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(conectionString);
+        }
+
+        protected int Execute(string sql, object obj)
+        {
+            using (MySqlConnection conexao = GetConnection())
+            {
+                return conexao.Execute(sql, obj);
+            }
+        }
     }
 }
