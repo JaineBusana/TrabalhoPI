69919e88f00e695af8453ce84b5ecf4bc9a5c9f6
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2023-11-23 16:07:27 -0300

    atualizando responsividade

--- site/cadastro-anuncio.js
+++ site/cadastro-anuncio.js
@@ -1,35 +1,25 @@
-const form = document.getElementById('form');
-const nome = document.getElementById('nome');
-const descrição = document.getElementById('descrição');
-const modalidade = document.getElementById('modalidade');
-const bairro = document.getElementById('bairro');
-const cep = document.getElementById('cep');
-const categoria = document.getElementById('categoria');
-const subcategoria = document.getElementById('subcategoria');
-const valor = document.getElementById('valor');
-const quantidade = document.getElementById('quantidade');
-const whastapp = document.getElementById('whatsapp');
-const telefone = document.getElementById('telefone');
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-})
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("form");
 
-function checkInputs() {
-    const nomeValue = nome.value;
-    const descriçãoValue = descrição.value;
-    const modalidadeValue = modalidade.value;
-    const bairroValue = bairro.value;
-    const cepValue = cep.value;
-    const categoriaValue = categoria.value;
-    const subcategoriaValue = subcategoria.value;
-    const valorValue = valor.value;
-    const whatsappValue = whastapp.value;
-    const telefoneValue = telefone.value;
+    form.addEventListener("submit", function (event) {
 
-    if(nomeValue == ''){
-       ("#nome") = "sucess";
+        const input = formulario.querySelectorAll("input[required]");
+        let inputPreenchido = true;
 
-    }
-}
+        input.forEach(function (input) {
+            if (input.value.trim() === "") {
+               
+                input.classList.add("error");
+                inputPreenchido = false;
+            } else {
+                input.classList.remove("error");
+            }
+        });
+
+        if (!inputPreenchido) {
+            event.preventDefault();
+        }
+    });
+});
 
