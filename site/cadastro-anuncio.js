ae692296eaabbfe0bc97ee14b5c588bfe4d82c75
LarissaBeatrizCosta <larissa.beatrizzcosta@gmail.com>
2023-11-28 19:16:25 -0300

    atualizando

--- site/cadastro-anuncio.js
+++ site/cadastro-anuncio.js
@@ -1,26 +1,179 @@
+const nome = document.querySelector(`#nome`)
+
+const descricao = document.querySelector(`#descrição`)
+
+const modalidade = document.querySelector(`#modalidade`)
+
+const bairro = document.querySelector(`#bairro`)
+
+const cep = document.querySelector(`#cep`)
+
+const categoria = document.querySelector(`#select-categoria`)
+
+const subcategoria = document.querySelector(`#subcategoria`)
+
+const valor = document.querySelector(`#valor`)
+
+const quantidade = document.querySelector(`#quantidade`)
+
+const telefone = document.querySelector(`#telefone`)
+
+const whatsapp = document.querySelector(`#whatsapp`)
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("form");
-
-    form.addEventListener("submit", function (event) {
-
-        const input = formulario.querySelectorAll("input[required]");
-        let inputPreenchido = true;
-
-        input.forEach(function (input) {
-            if (input.value.trim() === "") {
-               
-                input.classList.add("error");
-                inputPreenchido = false;
-            } else {
-                input.classList.remove("error");
-            }
-        });
 
 
-        if (!inputPreenchido) {
-            event.preventDefault();
-        }
-    });
-});
+const button = document.querySelector(`#btn-salvar`)
+
+const errorMessage = document.querySelector('.msg')
+
+
+
+
+
+
+button.addEventListener(`click`, (e) => {
+
+    e.preventDefault()
+
+
+
+
+    const nomeValor = nome.value;
+
+    const descricaoValor = descricao.value;
+
+    const modalidadeValor = modalidade.value;
+
+    const bairroValor = bairro.value;
+
+    const cepValor = cep.value;
+
+    const categoriaValor = categoria.value;
+
+    const subcategoriaValor = subcategoria.value;
+
+    const valorValor = valor.value;
+
+    const quantidadeValor = quantidade.value;
+
+    const telefoneValor = telefone.value;
+
+    const whatsappValor = whatsapp.value;
+
+
+
+
+    if (nomeValor === '' || descricaoValor === '' || modalidadeValor === '' ||
+
+        bairroValor === '' || cepValor === '' || categoriaValor === '' ||
+
+        subcategoriaValor === '' || valorValor === '' || quantidadeValor === '' ||
+
+        telefoneValor === '' || whatsappValor === '') {
+
+        errorMessage.textContent = "Preencha todos os campos!";
+
+        errorMessage.classList = "error";
+
+    }
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function submitForm(event) {
+
+
+
+
+    event.preventDefault();
+
+
+
+
+    var nome = document.forms["formulario"]["nome"].value;
+
+    var descricao = document.forms["formulario"]["descrição"].value;
+
+    var modalidade = document.forms["formulario"]["modalidade"].value;
+
+    var bairro = document.forms["formulario"]["bairro"].value;
+
+    var cep = document.forms["formulario"]["cep"].value;
+
+    var categoria = document.forms["formulario"]["select-categoria"].value;
+
+    var subcategoria = document.forms["formulario"]["subcategoria"].value;
+
+    var valor = document.forms["formulario"]["valor"].value;
+
+    var quantidade = document.forms["formulario"]["quantidade"].value;
+
+    var whatsapp = document.forms["formulario"]["whatsapp"].value;
+
+    var telefone = document.forms["formulario"]["telefone"].value;
+
+
+
+
+    var dataForm = {
+
+        nome: nome,
+
+        descricao: descricao,
+
+        modalidade: modalidade,
+
+        bairro: bairro,
+
+        cep: cep,
+
+        categoria: categoria,
+
+        subcategoria: subcategoria,
+
+        valor: valor,
+
+        quantidade: quantidade,
+
+        whatsapp: whatsapp,
+
+        telefone: telefone
+
+    };
+
+
+
+
+    var dataStore = localStorage.getItem('data') || '[]';
+
+    var dataArray = JSON.parse(dataStore);
+
+    dataArray.push(dataForm);
+
+    localStorage.setItem('data', JSON.stringify(dataArray));
+
+
+
+
+    console.log('Cadastro Salvo!', dataArray);
+
+}
+
--- site/index.js
+++ site/index.js
@@ -1,10 +1,10 @@
 function showModalMenu(classe) {
-    let modal = document.querySelector(classe)
+  let modal = document.querySelector(classe)
 
-    if (modal.classList.contains(`modal-active`)){
-      modal.classList.remove(`modal-active`)
-    }
-    else{
-      modal.classList.add(`modal-active`);
-    }       
+  if (modal.classList.contains(`modal-active`)){
+    modal.classList.remove(`modal-active`)
+  }
+  else{
+    modal.classList.add(`modal-active`);
+  }       
 }
