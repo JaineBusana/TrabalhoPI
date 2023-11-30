4c6b8836c35d5d1e8071ddc9ec445dd8829e5559
JaineBusana <jaine.ebbusanaf@gmail.com>
2023-11-30 20:26:33 -0300

    arrumando conflito

--- site/index.js
+++ site/index.js
@@ -1,25 +1,27 @@
-$(() => {
 function SHOW_MODAL_LOGIN() {
-    let modal = document.querySelector('#modal-login')
-    modal.classList.add(`modal-login-active`);
+  let modal = document.querySelector('#modal-login')
+  modal.classList.add(`modal-login-active`);
 }
 
 function CLOSE_MODAL_LOGIN() {
-    let modal = document.querySelector('#modal-login')
-    modal.classList.remove(`modal-login-active`);
+  let modal = document.querySelector('#modal-login')
+  modal.classList.remove(`modal-login-active`);
 }
 
 function showModalMenu(classe) {
   let modal = document.querySelector(classe)
-
+  
   if (modal.classList.contains(`modal-active`)){
     modal.classList.remove(`modal-active`)
   }
   else{
     modal.classList.add(`modal-active`);
   }       
+  
+  }
 
-}
+
+$(() => {
 
 $(".rodape-sugestoes-btn").click((e) => {
     e.preventDefault();
