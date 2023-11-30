630a113927ba39551ce2489aedbbe33bfe45f59e
JaineBusana <jaine.ebbusanaf@gmail.com>
2023-11-29 21:45:53 -0300

    iniciei o js

--- site/index.js
+++ site/index.js
@@ -1,3 +1,4 @@
+$(() => {
 function SHOW_MODAL_LOGIN() {
     let modal = document.querySelector('#modal-login')
     modal.classList.add(`modal-login-active`);
@@ -19,3 +20,29 @@ function showModalMenu(classe) {
   }       
 
 }
+
+$(".rodape-sugestoes-btn").click((e) => {
+    e.preventDefault();
+      let div = $('<div/>').addClass('error').html('Oi tudo bem!').insertAfter($('#modal-login'));      
+
+    const sugestao ={
+       textoSugestao: $("#rodape-sugestoes-textarea")[0].value
+
+    }
+
+    if (!sugestao.textoSugestao) {
+      
+      $("#nome").addClass("invalid");
+      return;
+  }
+
+
+
+    console.log(sugestao);
+
+})
+
+
+
+
+})
