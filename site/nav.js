b2f078a92fe029cb58dc4681093bea713549461e
thiago1237 <thiago.devjr@gmail.com>
2023-12-21 20:55:50 -0300

    g

--- site/nav.js
+++ site/nav.js
@@ -12,3 +12,21 @@ $(document).ready(function() {
     $(".owl-carousel").owlCarousel();
    
   });
+  $(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+   
+        navigation : true, // Show next and prev buttons
+   
+        slideSpeed : 300,
+        paginationSpeed : 400,
+   
+        items : 1, 
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false
+   
+    });
+   
+  });
