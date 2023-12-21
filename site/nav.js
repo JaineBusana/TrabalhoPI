6c4039368400b94c801a7638d53a3b211e22271e
thiago1237 <thiago.devje@gmail.com>
2023-12-21 16:54:20 -0300

    organizado pastas e iniciado nocas paginas html

---
+++ site/nav.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu')
+const NavHome = document.querySelector('.nav-home')
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+    NavHome.classList.toggle('ativo')
+})
