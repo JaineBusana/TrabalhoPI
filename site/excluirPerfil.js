98cbdfddd6cae1ba0cce30032ebb1e19a6c8a527
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-01-17 09:06:57 -0300

    adicionado imagem do planeta no modal

--- site/excluirPerfil.js
+++ site/excluirPerfil.js
@@ -5,6 +5,10 @@ function openModal() {
 }
 
 function closeModal() {
+    modal.classList.remove('active')
+  }
+
+function closeModalHome() {
   modal.classList.remove('active')
   window.location.href = "index-logado.html";
 }
