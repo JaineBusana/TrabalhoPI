286a20ebea3adadaad9be59030dda43d4a8d76d0
Matheus16777 <brizolamatheus61@gmail.com>
2024-01-17 21:14:47 -0300

    concluido a parte do BANCO DE DADOS(ENTITY,INFRASTRUCTURE)

---
+++ Trabalho_PI/Entity/UserEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_PI.Entity
+{
+    public class UserEntity
+    {
+        public int? Id { get; set; }
+        public string? Type { get; set; }
+        public string? Name { get; set; }
+        public string? CPF_CNPJ { get; set; }
+        public string? Email { get; set; }
+        public int? Telephone { get; set; }
+
+        public string? PassWord { get; set; }
+
+        public string? Score { get; set; }
+    }
+}
---
+++ Trabalho_PI/Infrastructure/Connection.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_PI.Infrastructure
+{
+    public class Connection
+    {
+        protected string connectionString = "Server=localhost;DataBase=Coleta_Blu;User=root;Password=root;";
+
+
+     protected MySqlConnection GetConnection()
+        {
+         return new MySqlConnection(connectionString);
+       }
+     protected async Task<int> Execute(string sql, object obj)
+        {
+         using (MySqlConnection con = GetConnection())
+            {
+             return await con.ExecuteAsync(sql,obj);
+            }
+        }
+    }
+}
