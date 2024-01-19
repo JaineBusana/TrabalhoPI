df2407b2628e2deef761b7f42d4525b76a31790c
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-01-19 15:13:41 -0300

    tela de sugestão com layout

---
+++ site/js/sugestao.js
@@ -0,0 +1,65 @@
+window.addEventListener("load", (event) => {
+
+    const form = document.getElementById("formSuggestion");
+    const textSuggestion = document.getElementById("textSuggestion")
+
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        checkForm();
+    })
+
+    textSuggestion.addEventListener("blur", () => {
+        checkTextSuggestion();
+    })
+
+   
+
+    function checkTextSuggestion() {
+        const textSuggestionValue = textSuggestion.value;
+
+        if (textSuggestionValue === "") {
+            errorInput(textSuggestion, " É necessário preencher o campo!")
+        } else {
+            const formItem = textSuggestion.parentElement;
+            formItem.className = "formContentSuggestion"
+        }
+
+    }
+
+
+    function checkForm() {
+        checkTextSuggestion();
+       
+        const sucessMessage = document.querySelector('.msgSuggestion')
+
+        const formItems = form.querySelectorAll(".formContentSuggestion")
+
+        const isValid = [...formItems].every((item) => {
+            return item.className === "formContentSuggestion"
+        });
+
+        if (isValid) {
+
+            sucessMessage.innerHTML = "Sugestão Enviada!";
+            sucessMessage.classList.add("sucess");
+
+            setTimeout(() => {
+                sucessMessage.innerHTML = "";
+                sucessMessage.classList.remove("sucess");
+            }, 5000);
+        }
+
+    }
+
+    function errorInput(textArea, message) {
+        const formItem = textArea.parentElement;
+        const textMessage = formItem.querySelector("a")
+
+        textMessage.innerText = message;
+
+        formItem.className = "formContentSuggestion error"
+
+    }
+});
