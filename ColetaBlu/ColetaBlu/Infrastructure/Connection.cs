f42106e76333e9565d8648976ee879df51c0d789
Matheus16777 <brizolamatheus61@gmail.com>
2024-01-18 21:41:06 -0300

    Criei o projeto COLETABLU

---
+++ ColetaBlu/ColetaBlu/Infrastructure/Connection.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace ColetaBlu.Infrastructure
+{
+    public class Connection
+    {
+        protected string connectionString = "Server=localhost;DataBase=Coleta_Blu;User=root;Password=root";
+       
+        protected MySqlConnection GetConnection()
+        {
+         return new MySqlConnection(connectionString);
+        }
+        protected async Task<int> Execute(string sql, object obj)
+        {
+         using (MySqlConnection con = GetConnection())
+            {
+                return await con.ExecuteAsync(sql, obj);
+            }
+        }
+    }
+}
