4fc5ee2e0668c8538dd34e77cf7921a47a4ae901
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-01-23 13:28:31 -0300

    avançado validação de modal do Registro

--- site/js/registroColeta.js
+++ site/js/registroColeta.js
@@ -8,124 +8,27 @@ function closeModalHistoric() {
     modal.classList.remove('active')
 }
 
-
 window.addEventListener("load", (event) => {
 
+    const identifier = document.getElementById("placeIdentifier")
 
-    const formHistoric = document.getElementById("formHistoric");
-    const placeIdentifier = document.getElementById("placeIdentifier")
-    // const address = document.getElementById("address")
-    // const phoneNumber = document.getElementById("phoneNumber")
-    // const residue = document.getElementById("selectResidue");
-
-
-    formHistoric.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        checkForm();
+    identifier.addEventListener("blur", () => {
+        checkIdentifier();
     })
 
-    // address.addEventListener("blur", () => {
-    //     checkInputAddress();
-    // })
+    function checkIdentifier() {
+        const identifierValue = identifier.value;
 
-
-    placeIdentifier.addEventListener("blur", () => {
-        checkInputPlaceIdentifier();
-    })
-
-    // phoneNumber.addEventListener("blur", () => {
-    //     checkInputPhoneNumber();
-    // })
-
-    // residue.addEventListener("blur", () => {
-    //     checkInputResidue();
-    // })
-
-
-    function checkInputPlaceIdentifier() {
-        const placeIdentifierValue = placeIdentifier.value;
-
-        if (placeIdentifierValue === "") {
-            errorInput(placeIdentifier, " CPF não preenchido!")
+        if (identifierValue === "") {
+            errorInput(identifier, "CPF não preenchido!")
+        } else if (identifierValue.length < 11) {
+            errorInput(identifier, "O CPF precisa ter  11 CARACTERES.")
         } else {
-            const formItem = placeIdentifier.parentElement;
-            formItem.className = "formHistoricContent"
+            const formItem = identifier.parentElement;
+            formItem.className = "modalHistoric"
         }
 
     }
-
-    // function checkInputAddress() {
-    //     const addressValue = address.value;
-
-    //     if (addressValue === "") {
-    //         errorInput(address, "ENDEREÇO não preenchido!")
-    //     } else {
-    //         const formItem = address.parentElement;
-    //         formItem.className = "formHistoricContent"
-    //     }
-
-
-    // }
-
-
-    // function checkInputPhoneNumber() {
-    //     const phoneNumberValue = phoneNumber.value;
-
-    //     if (phoneNumberValue === "") {
-    //         errorInput(phoneNumber, "NÚMERO DE CONTATO não preenchido!")
-    //     } else if (phoneNumberValue.length < 9) {
-    //         errorInput(phoneNumber, "O número de contato precisa ter  9 CARACTERES.")
-    //     } else {
-    //         const formItem = phoneNumber.parentElement;
-    //         formItem.className = "formHistoricContent"
-    //     }
-
-
-    // }
-
-
-    // function checkInputResidue() {
-    //     const residueValue = residue.value;
-
-    //     if (residueValue === "") {
-    //         errorInput(residue, "RESÍDUO não preenchido!")
-    //     } else {
-    //         const formItem = residue.parentElement;
-    //         formItem.className = "formHistoricContent"
-    //     }
-
-
-    // }
-
-
-    function checkForm() {
-        checkInputPlaceIdentifier();
-        // checkInputAddress();
-        // checkInputPhoneNumber();
-        // checkInputResidue();
-        const sucessMessage = document.querySelector('.msgHistoric')
-
-        const formItems = formHistoric.querySelectorAll(".formHistoricContent")
-
-        const isValid = [...formItems].every((item) => {
-            return item.className === "formHistoricContent"
-        });
-
-        if (isValid) {
-
-            sucessMessage.innerHTML = "Confirmado!";
-            sucessMessage.classList.add("sucess");
-
-            setTimeout(() => {
-                sucessMessage.innerHTML = "";
-                sucessMessage.classList.remove("sucess");
-            }, 5000);
-            closeModalHistoric();
-        }
-
-    }
-
 
     function errorInput(input, message) {
         const formItem = input.parentElement;
@@ -133,7 +36,8 @@ window.addEventListener("load", (event) => {
 
         textMessage.innerText = message;
 
-        formItem.className = "formHistoricContent error"
+        formItem.className = "modalHistoric error"
 
     }
+
 });
