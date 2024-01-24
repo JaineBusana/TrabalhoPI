72cd6d63b32f988f0da611cdc680c09abcc8541d
LarissaBeatrizCosta <larissa.beatrizzcosta@gmail.com>
2024-01-23 21:19:08 -0300

    registro form

--- site/js/registroColeta.js
+++ site/js/registroColeta.js
@@ -1,6 +1,5 @@
-const modal = document.querySelector('.modalContainerHistoric')
-const modalRegister = document.querySelector('.modalContainerHistoric')
-const secondModal = document.querySelector('.register');
+const modal = document.querySelector('.containerHistory')
+
 
 function openModalHistoric() {
     modal.classList.add('active')
@@ -9,44 +8,3 @@ function openModalHistoric() {
 function closeModalHistoric() {
     modal.classList.remove('active')
 }
-
-function openModalRegister() {
-    modal.classList.add('active')
-}
-
-window.addEventListener("load", (event) => {
-
-    const identifier = document.getElementById("placeIdentifier")
-
-    identifier.addEventListener("blur", () => {
-        checkIdentifier();
-    })
-
-    function checkIdentifier() {
-        const identifierValue = identifier.value;
-
-        if (identifierValue === "") {
-            errorInput(identifier, "CPF não preenchido!")
-        } else if (identifierValue.length < 11) {
-            errorInput(identifier, "O CPF precisa ter  11 CARACTERES.")
-        } else {
-            const formItem = identifier.parentElement;
-            formItem.className = "modalHistoric"
-            secondModal.style.display = "flex";
-            modal.style.display = "none";
-
-        }
-
-    }
-
-    function errorInput(input, message) {
-        const formItem = input.parentElement;
-        const textMessage = formItem.querySelector("a")
-
-        textMessage.innerText = message;
-
-        formItem.className = "modalHistoric error"
-
-    }
-
-});
