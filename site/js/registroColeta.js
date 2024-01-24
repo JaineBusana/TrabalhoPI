1c96fd9c4a3fd5ccca83b51ab28d6b38e5bb8a3d
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-01-23 23:31:56 -0300

    add inputs ao clicar no botão do modal Registros

--- site/js/registroColeta.js
+++ site/js/registroColeta.js
@@ -1,3 +1,5 @@
+const form = document.getElementById('formHistoric');
+const btnAddHistoric = document.getElementById('btnAddHistoric');
 const modal = document.querySelector('.containerHistoric')
 
 
@@ -8,3 +10,34 @@ function openModalHistoric() {
 function closeModalHistoric() {
     modal.classList.remove('active')
 }
+
+
+
+btnAddHistoric.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const addInputHistoric = document.createElement('div');
+
+    addInputHistoric.classList.add('formContentHistoric');
+
+    const divHistoric = `
+    <div class="addNewHistoric">
+    <div class="formContentHistoric">
+        <label for="placeNameHistoric">Nome do Produto</label>
+        <input type="text" id="placeNameHistoric" placeholder="Digite aqui..." />
+        <a>Mensagem de Erro</a>
+    </div>
+
+    <div class="formContentHistoric">
+        <label for="placeQuantityHistoric">Quantidade</label>
+        <input type="text" id="placeQuantityHistoric" placeholder="Digite aqui..." />
+        <a>Mensagem de Erro</a>
+    </div>
+</div>
+
+    `;
+
+    addInputHistoric.innerHTML = divHistoric;
+
+    form.insertBefore(addInputHistoric, form.lastElementChild);
+});
