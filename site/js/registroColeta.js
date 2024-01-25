5922282085003d3402f49b3171a6d2f07e2b3ddc
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-01-25 10:46:52 -0300

    impedimento de validação de quantidade resolvidotela Registros

--- site/js/registroColeta.js
+++ site/js/registroColeta.js
@@ -9,34 +9,31 @@ const modal = document.querySelector('.containerHistoric');
 const btnCloseHistoric = document.getElementById('btnCloseHistoric');
 
 
+
 // ABRE E FECHA MODAL⬇️
 function openModalHistoric() {
     modal.classList.add('active')
 }
 
 
+
+
 // DUPLICA OS INPUTS ⬇️  !!!!!! NOMES DOS INPUTS MULTIPLICAM
 btnAddHistoric.addEventListener('click', function (event) {
+
     event.preventDefault();
 
     const addInputHistoric = document.createElement('div');
-
     addInputHistoric.classList.add('formContentHistoric');
 
 
     const divHistoric = `
     <div class="addNewHistoric">
     <div class="formContentHistoric">
-    <label for="placeNameHistoric">Nome do Produto</label>
-    <select class="placeNameHistoric">
-    <option value="pet">Selecione</option>
-    <option value="pet">Lata de Cerveja</option>
-    <option value="pet">Long Neck</option>
-    <option value="pet">Garrafa Pet</option>
-    </select>
+        <label for="placeNameHistoric">Nome do Produto</label>
+        <input type="text" class="placeNameHistoric" placeholder="Digite aqui..." />
         <a>Mensagem de Erro</a>
     </div>
-
     <div class="formContentHistoric">
         <label for="placeQuantityHistoric">Quantidade</label>
         <input type="text" id="placeQuantityHistoric" placeholder="Digite aqui..." />
@@ -44,95 +41,81 @@ btnAddHistoric.addEventListener('click', function (event) {
     </div>
 </div>`;
 
+
     addInputHistoric.innerHTML = divHistoric;
-
     form.insertBefore(addInputHistoric, form.lastElementChild);
-
-
 });
+
+
 
 // VALIDAÇÃO ⬇️
 window.addEventListener("load", (event) => {
 
     const placeCpfHistoric = document.getElementById("placeCpfHistoric")
-    // const placeNameHistoric = document.getElementById("placeNameHistoric");
+    const placeNameHistoric = document.querySelector(".placeNameHistoric");
     const placeQuantityHistoric = document.getElementById("placeQuantityHistoric");
 
+
     form.addEventListener("submit", (e) => {
+
         e.preventDefault();
 
         checkForm();
     })
 
+
     placeCpfHistoric.addEventListener("blur", () => {
         checkInputCPF();
     })
 
-    // placeNameHistoric.addEventListener("blur", () => {
-    //     checkInputName();
-    // })
 
+    placeNameHistoric.addEventListener("blur", () => {
+        checkInputName();
+    })
 
     placeQuantityHistoric.addEventListener("blur", () => {
         checkInputQuantity();
     })
 
-
     function checkInputCPF() {
-
         const cpfValue = placeCpfHistoric.value;
         if (cpfValue === "") {
             errorInput(placeCpfHistoric, "Não preenchido!")
-
         } else if (cpfValue.length < 11) {
             errorInput(placeCpfHistoric, "O CPF precisa ter  11 CARACTERES.")
         } else {
             const formItem = placeCpfHistoric.parentElement;
             formItem.className = "formContentHistoric"
-
         }
-
     }
 
-    // function checkInputName() {
-
-    //     const nameValue = placeNameHistoric.value;
-    //     if (nameValue === "") {
-    //         errorInput(placeNameHistoric, "Não preenchido!")
-
-    //     } else {
-    //         const formItem = placeNameHistoric.parentElement;
-    //         formItem.className = "formContentHistoric"
-
-    //     }
-
-    // }
+    function checkInputName() {
+        const nameValue = placeNameHistoric.value;
+        if (nameValue === "") {
+            errorInput(placeNameHistoric, "Não preenchido!")
+        } else {
+            const formItem = placeNameHistoric.parentElement;
+            formItem.className = "formContentHistoric"
+        }
+    }
 
     function checkInputQuantity() {
-
         const quantityValue = placeQuantityHistoric.value;
         if (quantityValue === "") {
             errorInput(placeQuantityHistoric, "Não preenchida!")
-
         } else {
             const formItem = placeQuantityHistoric.parentElement;
             formItem.className = "formContentHistoric"
-
         }
-
     }
+
     function checkForm() {
 
-        $('.placeNameHistoric').select2();
-
-        
         checkInputCPF();
         checkInputName();
         checkInputQuantity();
 
         const formItems = form.querySelectorAll(".formContentHistoric")
-
-        
         const isValid = [...formItems].every((item) => {
             return item.className === "formContentHistoric"
         });
@@ -142,8 +125,8 @@ window.addEventListener("load", (event) => {
             alert("Registro Salvo!");
             window.location.reload();
         }
-
     }
+
 
     function errorInput(input, message) {
         const formItem = input.parentElement;
@@ -151,16 +134,11 @@ window.addEventListener("load", (event) => {
         textMessage.innerText = message;
         formItem.className = "formContentHistoric error"
     }
-
-
-
 });
 
 function removeInput() {
     const inputNew = document.querySelector(".addNewHistoric");
-
     if (inputNew) {
         inputNew.remove();
     }
 }
-
