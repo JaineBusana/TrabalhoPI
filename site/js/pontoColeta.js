f997093395d343c4fa86bf250e8ab9a287ddfdf3
Aline <alinee.kowalski@gmail.com>
2024-02-04 21:46:49 -0300

    PONTO COLETA .JS - ALTERAÇÕES

--- site/js/pontoColeta.js
+++ site/js/pontoColeta.js
@@ -1,13 +1,18 @@
 const neighborhood = {
-    0:null,
-    1:{nome:"Badenfurt", endereco:"UFSC", observacao:"Fundos" },
-    2:{nome: "Fidélis", endereco:"Rua imperatriz leopoldina 123", observacao:"em frente ao mercado"},
-    3:{nome:"Itoupava central", endereco:"Itoupava central", observacao:"Farmácia"}
-}
+    0: null,
+    1: { nome: "Badenfurt", endereco: "UFSC", observacao: "Fundos" },
+    2: { nome: "Fidélis", endereco: "Rua imperatriz leopoldina 123", observacao: "em frente ao mercado" },
+    3: { nome: "Itoupava central", endereco: "Itoupava central", observacao: "Farmácia" }
+};
 
-const selectNeighborhood =()=>{
-    const select = document.getElementById("selectNeighborhood")
-    const selectedValue = select.value
+const selectNeighborhood = () => {
+
+};
+
+const btnLocalPonto = document.getElementById("btnLocalPonto");
+btnLocalPonto.addEventListener("click", () => {
+    const select = document.getElementById("selectNeighborhood");
+    const selectedValue = select.value;
 
     const nameCardElement = document.getElementById("nameCard");
     const addressElement = document.querySelector(".address");
@@ -17,18 +22,16 @@ const selectNeighborhood =()=>{
         const neighborhoodSelected = neighborhood[selectedValue];
 
         if (neighborhoodSelected) {
-         
             nameCardElement.textContent = neighborhoodSelected.nome;
-            addressElement.textContent = `${neighborhoodSelected.endereco}`;
-            obsCardElement.textContent = `${neighborhoodSelected.observacao}`;
+            addressElement.textContent = "Endereço:" `${neighborhoodSelected.endereco}`;
+            obsCardElement.textContent = "Observação:" `${neighborhoodSelected.observacao}`;
         }
     } else {
         nameCardElement.textContent = "";
         addressElement.textContent = "Endereço: ";
         obsCardElement.textContent = "Observação: ";
     }
+});
 
- 
-}
 const selectNeighborhoodValue = document.getElementById("selectNeighborhood");
-selectNeighborhoodValue.addEventListener("change", selectNeighborhood )
+selectNeighborhoodValue.addEventListener("change", selectNeighborhood);
