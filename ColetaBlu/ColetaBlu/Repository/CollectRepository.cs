7f2623616370e0ff43e61bc37932b3d381492b0a
Jaine Busana <jaine.ebbusanaf@gmail.com>
2024-02-08 21:47:26 -0300

    arrumei o connection

--- ColetaBlu/ColetaBlu/Repository/CollectRepository.cs
+++ ColetaBlu/ColetaBlu/Repository/CollectRepository.cs
@@ -5,20 +5,20 @@ using ColetaBlu.Infrastructure;
 
 namespace ColetaBlu.Repository
 {
-    public class CollectRepository : Connection, ICollectRepository
-    {
-        public Task Add(CollectDTO collect)
-        {
-            string sql = @"
-                INSERT INTO Collect (Name, Street, Number, Neighborhood_Id, User_Id)
-                            VALUE (@Name, @Street, @Number, @Neighborhood_Id, @User_Id)
-            ";
-            await Execute(sql, cadastroPonto);
-        }
+    //public class CollectRepository : Connection, ICollectRepository
+    //{
+    //    public Task Add(CollectDTO collect)
+    //    {
+    //        string sql = @"
+    //            INSERT INTO Collect (Name, Street, Number, Neighborhood_Id, User_Id)
+    //                        VALUE (@Name, @Street, @Number, @Neighborhood_Id, @User_Id)
+    //        ";
+    //        await Execute(sql, cadastroPonto);
+    //    }
 
-        public Task<IEnumerable<CadastroPontoEntity>> Read()
-        {
-            throw new NotImplementedException();
-        }
-    }
+    //    public Task<IEnumerable<CadastroPontoEntity>> Read()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
 }
