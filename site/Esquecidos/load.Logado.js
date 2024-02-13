27e4089570c67ad139fda8e8edd2a928bb5d1c1c
Lariss Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2024-02-13 09:39:21 -0300

    add arquivos da spa no esquecidos

---
+++ site/Esquecidos/load.Logado.js
@@ -0,0 +1,8 @@
+$(".navItem").click(function () {
+    if (!$(this).data('html')) {
+        return;
+    }
+    $.get($(this).data('html'), (result) => {
+        $('main').html(result);
+    })
+});
