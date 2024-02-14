2190da738c6cc474bc7a5699ed25496cb7e884d7
Matheus16777 <brizolamatheus61@gmail.com>
2024-02-14 19:50:23 -0300

    Adicionei a parte da logica de mudar a NAV

--- site/js/login.js
+++ site/js/login.js
@@ -1,46 +1,79 @@
-function SHOW_MODAL_LOGIN() {
-    let modal = document.querySelector('#modalLogin')
-    modal.classList.add(`modalLoginActive`);
-}
-
-function CLOSE_MODAL_LOGIN() {
-    let modal = document.querySelector('#modalLogin')
-    modal.classList.remove(`modalLoginActive`);
-}
-
 window.addEventListener("load", (event) => {
- 
+
     const usuario = document.querySelector(`#insertUserLogin`)
     const senha = document.querySelector(`#insertPasswordLogin`)
- 
+
     const button = document.querySelector(`#login`)
- 
- 
-    button.addEventListener(`click`, (e) => {
+
+
+    button.addEventListener(click, (e) => {
         e.preventDefault()
- 
+
         const errorMessage = document.querySelector('.msg')
- 
+
         const value = {
             usuario: usuario.value,
             senha: senha.value,
         }
- 
- 
+
+
         if (value.usuario === '' || value.senha === '') {
- 
+
             errorMessage.innerHTML = "Preencha todos os campos";
             errorMessage.classList.add("error");
-           
+
             setTimeout(() => {
                 errorMessage.innerHTML = "";
                 errorMessage.classList.remove("error");
-            },3000);
+            }, 3000);
         }
         else {
-            window.location = "index-logado.html"
+            $(() => {
+
+                if (!!localStorage.getItem(token)) {
+                    location.href = "indexLogado.html";
+                }
+                $("#login").click((e) => {
+                    e.preventDefault();
+
+                    const data = {
+                        email: $("#insertUserLogin").val(),
+                        password: $("#insertPasswordLogin").val()
+                    };
+
+
+
+                    $.ajax({
+                        type: "POST",
+                        url: "https://localhost:7249/user/login",
+                        data: JSON.stringify(data),
+                        success: (result) => {
+                            localStorage.clear();
+                            localStorage.setItem(token, result.token);
+                            localStorage.setItem(userName, result.user.name);
+                            localStorage.setItem(userRole, result.user.role);
+                            location.href = "indexLogado.html";
+                        },
+                        contentType: "application/json",
+                        dataType: "json",
+                    });
+                });
+            });
         }
-        
+   
     })
+    localStorage.getItem(userRole);
+    if (userRole == "cidadao") {
+        var url = "https://www.google.com/?/links";
+        openLink(url);
+    }
+    if (result.user.role == "coletor") {
+        var url = "https://www.bing.com/"
+        openLink(url);
+    }
+
+    function openLink(url) {
+        var win = window.open(url, '_blank');
+        win.focus();
+    }
 })
-        
