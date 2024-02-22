e4d140697908a0338a4058b1ac5032b7b4a14705
GustaPandini <gugui.pandini@gmail.com>
2024-02-21 21:44:16 -0300

    Removido codigos não usados

--- site/js/perfil.js
+++ site/js/perfil.js
@@ -1,10 +1,4 @@
-const menu = document.querySelector('.menu');
-const NavMenu = document.querySelector('.nav-menu')
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo');
-})
 
 function OPEN_MODAL_PERFIL() {
     modalPerfil.style.display = 'flex';
